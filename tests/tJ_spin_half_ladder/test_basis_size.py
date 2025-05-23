dad76c0073d868bfe6400d150dcb889f3caeccea
imnotgonnasithereandtryallday <adamus.petr@seznam.cz>
2025-05-23 17:09:39 +0200

    +sum over k, leg periodicity, and negative leg momenta tests

--- tests/tJ_spin_half_ladder/test_basis_size.py
+++ tests/tJ_spin_half_ladder/test_basis_size.py
@@ -4,6 +4,7 @@ import glob
 from typing import Self
 from pydantic.dataclasses import dataclass
 
+import scipy.special
 
 import exactdiag.tJ_spin_half_ladder.configs as lc
 
@@ -20,12 +21,67 @@ def test_against_precalculated():
         assert pre.basis_size == py_basis_map.get_num_states(), (file, pre.basis_size, py_basis_map.get_num_states())
 
 
-def test_negative_k():
-    pass
+def test_negative_k_leg():
+    """Basis size should be the same for k as for (-k[0], k[1])."""
+    # The system chosen to have varied number of states for different momenta.
+    collected = collect(num_rungs=6, num_holes=0, num_down_spins=6)
+    expected_num_files = 8
+    assert len(collected) == expected_num_files
+    for params in collected:
+        k = params.symmetry_qs
+        if k[0] == 0:
+            continue
+        config = params.to_hamiltonian_config()
+        config.symmetry_qs.leg *= -1
+        _, py_basis_map, _ = config.get_translators()
+        assert py_basis_map.get_num_states() == params.basis_size
+
+
+def test_k_leg_periodicity():
+    """Basis size should remain the same when shifting k by a multiple of its periodicity."""
+    # NOTE: We do not test config.symmetry_qs.rung, as that is validated to be in {0, 1}.
+    # The system chosen to have varied number of states for different momenta.
+    collected = collect(num_rungs=6, num_holes=0, num_down_spins=6)
+    expected_num_files = 8
+    assert len(collected) == expected_num_files
+    multiples = range(-4, 5)
+    for params in collected:
+        k = params.symmetry_qs
+        if k == (0, 0):
+            continue
+        config = params.to_hamiltonian_config()
+        periodicities = tuple(config.periodicities)
+        for leg_multiple in multiples:
+            config.symmetry_qs.leg = k[0] + leg_multiple * periodicities[0]
+            _, py_basis_map, _ = config.get_translators()
+            assert py_basis_map.get_num_states() == params.basis_size
 
 
 def test_sum_over_k():
-    pass
+    """Summed number of states should give values calculated using combinatorics."""
+    # NOTE: We test the saved files, nothing is calculated.
+    # We assume that there are no files with negative or reducible momenta.
+    # Counting negative momenta: all our files contribute twice except
+    # those with k_leg == 0 or (k_leg == num_rungs//2 if num_rungs is even).
+    collected = collect_all()
+    rungs_holes_down_to_pre = {}
+    for fpath in collected:
+        pre = Precalculated.load(fpath)
+        rungs_holes_down = (pre.num_rungs, pre.num_holes, pre.num_down_spins)
+        if rungs_holes_down not in rungs_holes_down_to_pre:
+            rungs_holes_down_to_pre[rungs_holes_down] = []
+        multiplier = 1 + (pre.symmetry_qs[0] != 0 and (pre.symmetry_qs[0] != pre.num_rungs // 2 or pre.num_rungs % 2))
+        rungs_holes_down_to_pre[rungs_holes_down].append(multiplier * pre.basis_size)
+
+    rungs_holes_down_to_pre = {
+        k: sizes for k, sizes in rungs_holes_down_to_pre.items() if len(sizes) == 2 * (k[0] // 2 + 1)
+    }
+    expected_num_sets = 7
+    assert len(rungs_holes_down_to_pre) == expected_num_sets
+    for (num_rungs, num_holes, num_down_spins), sizes in rungs_holes_down_to_pre.items():
+        num_spin_states = scipy.special.comb(2 * num_rungs - num_holes, num_down_spins, exact=True)
+        num_hole_states = scipy.special.comb(2 * num_rungs, num_holes, exact=True)
+        assert num_spin_states * num_hole_states == sum(sizes)
 
 
 def test_sum_over_holes():
@@ -43,14 +99,16 @@ def test_equivalent_systems():
 def test_inter_symmetry():
     """General and specialized symmetries should give the same basis sizes."""
 
+
 def collect_all():
+    # FIXME: Shares name with collect() but the return type differs.
     folder = pathlib.Path(__file__).parent / "precalculated"
     return glob.glob(f"{(folder / '*')!s}")
 
 
 def collect(num_rungs: int, num_holes: int, num_down_spins: int):
     folder = pathlib.Path(__file__).parent / "precalculated"
-    mkx = num_rungs // 2
+    mkx = num_rungs // 2 + 1
     mky = 2
     collected = []
     for kx in range(mkx):
